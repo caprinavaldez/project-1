9850ca70f5671a5541fa783cb2e9d01c4748bc9d
caprinavaldez <caprinavaldez@gmail.com>
2017-11-19 15:54:27 -0800

    more map stuff

--- Date saver/map.js
+++ Date saver/map.js
@@ -1,29 +1,58 @@
 $(document).ready(function(){
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDZWrLDyGkCd42-ATvbmoSldvqx44YsgnM",
-    authDomain: "date-saver.firebaseapp.com",
-    databaseURL: "https://date-saver.firebaseio.com",
-    projectId: "date-saver",
-    storageBucket: "date-saver.appspot.com",
-    messagingSenderId: "169298127003"
-  };
+
+	// $('#map').hide();
+
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDZWrLDyGkCd42-ATvbmoSldvqx44YsgnM",
+		authDomain: "date-saver.firebaseapp.com",
+		databaseURL: "https://date-saver.firebaseio.com",
+		projectId: "date-saver",
+		storageBucket: "date-saver.appspot.com",
+		messagingSenderId: "169298127003"
+	};
   
 	firebase.initializeApp(config);
 
 	var database = firebase.database();
 
-	$("#download-button").on('click', function(event){
+	$("#download-button").on("click", function(event){
+		
 		event.preventDefault();
 
-		var userInput = $("#last_name").val().trim();
-		console.log(userInput);
+		var userInput = $("#location").val().trim();
+		console.log("User Input: " + userInput);
 
-		database.ref().push({
+		database.ref().set({
 			location: userInput
 		});
 
-		location = $("#last_name").val("");
+		location = $("#location").val("");
 
 	})
+
+	database.ref().on("value", function(snapshot) {
+		console.log("userInput Database: " + snapshot.val().location);
+			
+		// $('#maps').hide();
+		// $('#map').show();
+
+		var userLocation = $(this);
+		var queryURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAJECaqqnTXASdBgV2knhhTF9XVV_DfqYM&q=" + userLocation; 
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(response) {
+			console.log(response);
+
+			var locationMap = $("<iframe>")
+				.attr("width='600' height='450'")
+				.attr("frameborder='0' style='border:0'")
+				.replace("src=" + queryURL);
+			
+			$('#maps').replaceHTML(locationMap)
+		})		
+	})
+
 })
