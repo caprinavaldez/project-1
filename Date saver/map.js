71a90f11369808a2781d62e138b11afc76e77d07
caprinavaldez <caprinavaldez@gmail.com>
2017-11-27 18:47:24 -0800

    map trying again

--- Date saver/map.js
+++ Date saver/map.js
@@ -2,20 +2,6 @@ $(document).ready(function(){
 
 	$('#map').hide();
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyDZWrLDyGkCd42-ATvbmoSldvqx44YsgnM",
-		authDomain: "date-saver.firebaseapp.com",
-		databaseURL: "https://date-saver.firebaseio.com",
-		projectId: "date-saver",
-		storageBucket: "date-saver.appspot.com",
-		messagingSenderId: "169298127003"
-	};
-  
-	firebase.initializeApp(config);
-
-	var database = firebase.database();
-
 	$("#download-button").on("click", function(event){
 		
 		event.preventDefault();
