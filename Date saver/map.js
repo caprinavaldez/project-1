10d45888280106a85331132ee7e84fa244817668
Unknown <nguyen.frank.minh@gmail.com>
2017-11-29 21:16:45 -0800

    Auto stash before merge of "master" and "origin/master"

--- Date saver/map.js
+++ Date saver/map.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
 
-//map js
+	//MAP JS
+
 	$('#map').hide();
 	$("#jokeSpace").hide();
 	$('#jokeButton').hide();
-
 	$("#download-button").on("click", function(event){
-		
 		event.preventDefault();
-
 		$('#maps').hide();
 		$('#jokeButton').show();
 
@@ -19,13 +17,11 @@ $(document).ready(function(){
 		$('#map').show().attr('src', queryURL);
 
 		getFood();
-
 	})
 
 //joke js
 	$("#jokeButton").on("click",function(){
 		var randomJoke = $(this).data("search");
-		
 		console.log(randomJoke);
 
 		var queryURL = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
@@ -37,15 +33,16 @@ $(document).ready(function(){
 
 				$("#jokeSpace").html("<p>Joke: "+response.setup+"</p>"+"<p>Punchline: "+response.punchline+"</p>");
 		})
-
 		$("#jokeSpace").show();
 	})
 
 //food js
+
     function getFood(){
-        $('.foodResponse').html("");
+		$('.foodResponse').html("");
         var cityName = $('#location').val().trim();
         console.log(cityName);
+<<<<<<< Updated upstream
         var apiCall = 'https://developers.zomato.com/api/v2.1/search?entity_id=484&entity_type=city&q=' + cityName+ '&count=4&sort=rating'; 
         
    $.ajax({
@@ -119,4 +116,37 @@ $(document).ready(function(){
 		$("#firebase").prepend("<li>" + userInput);
 
 	});
+=======
+        var apiCall = 'https://developers.zomato.com/api/v2.1/search?entity_id=484&entity_type=city&q=' + cityName+ '&count=5&sort=rating'; 
+        $.ajax({
+			type: "GET",
+      		url: apiCall,
+      		headers: {
+				  "user-key": "fd9d28b4d8120ec5f127e18a9d9a620e"
+				}
+			}).done((data) => {
+				console.log(data);
+				foodCallBack(data)
+			})
+			function foodCallBack(foodData){
+				var restaurantList = foodData.restaurants;
+				for (i = 0; i < restaurantList.length; i++) {
+					var foodRating = foodData.restaurants[i].restaurant.user_rating.aggregate_rating;
+					var foodName = foodData.restaurants[i].restaurant.name;
+					var foodCost = foodData.restaurants[i].restaurant.average_cost_for_two;
+					var foodTopic = foodData.restaurants[i].restaurant.cuisines;
+					var foodURL = foodData.restaurants[i].restaurant.url;
+
+					console.log("Rating: " + foodRating);
+					console.log("Name: " + foodName);
+					console.log("Cost for Two: " + foodCost);
+					console.log("Topic: " + foodTopic);
+					console.log("URL: " + foodURL);
+
+					$('.foodResponse').append("<div>" + "Restaurant: " + foodName + ' Rating: ' + foodRating + " Cost for Two: " + foodCost + " Topic: " + foodTopic + " Link: " + foodURL);
+				}
+				$('#foodtitle').append(foodData.restaurants[0].restaurant.name;);
+			}
+		}
+>>>>>>> Stashed changes
 })
